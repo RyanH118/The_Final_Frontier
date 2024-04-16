124c0bfbebe8971ecebd80972396e4da65971047
ryanh <ryanhinson118@mail.com>
2024-04-16 17:11:29 -0400

    Added images API to show 10 results on image HTML.

--- assets/JS/image.js
+++ assets/JS/image.js
@@ -1,26 +1,39 @@
 var apiKey = 'gL5Az0Z7i2Yvl2kLUQ9azhjjV2iGWkX5TOr7Zjp6';
-const keywords = "star"
-const apiUrl = `https://images-api.nasa.gov/search?q=${keywords}&media_type=image`;
 
+function searchNasaImages() {
+    const keyword = document.getElementById("searchInput").value;
+    const url = `https://images-api.nasa.gov/search?q=${keyword}&media_type=image&page=1&page_size=10&`;
 
-// Make a GET request to the API
-fetch(apiUrl)
-  .then(response => {
-    // Check if the response is successful
-    if (!response.ok) {
-      throw new Error('no worky try again');
-    }
-    // Parse the JSON response
-    return response.json();
-  })
-  .then(imageInfo => {
-    // Work with the JSON data
-    console.log(imageInfo);
-  })
-  .catch(error => {
+    // Make a GET request to the API
+    fetch(url)
+        // Check if the response is successful
+        .then(function (response) {
+            return response.json();
+            // Parse the JSON response
+        })
+        .then(function (data) {
+            const imageContainer = document.getElementById("imageContainer");
+            imageContainer.innerHTML = ""; // clear existing images
+
+            data.collection.items.forEach(item => {
+                // Work with the JSON data
+                const title = item.data[0].title;
+                const image = item.links[0].href;
+                const description = item.data[0].description;
+
+                const imageDiv = document.createElement("div");
+                imageDiv.classList.add("image-container");
+                imageDiv.innerHTML = `
+            <h2>${title}</h2>
+            <img src= "${image}"></img>
+            <p>${description}</p>`
+
+                imageContainer.appendChild(imageDiv);
+            });
+        })
+        .catch(error => console.error(error));
     // Handle any errors that occurred during the fetch
-    console.error('Error fetching data:', error);
-  });
+}
 // when we search for an item take the key word and put it into a string.
 // fetch image api and put it into query parameters
 // append image and desc from api
