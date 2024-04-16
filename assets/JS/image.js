1d43f63e284f77345485f75c643009d042d38253
ryanh <ryanhinson118@mail.com>
2024-04-16 14:17:33 -0400

    Added a search bar to images html for images api to ctach keywords.

--- assets/JS/image.js
+++ assets/JS/image.js
@@ -21,7 +21,6 @@ fetch(apiUrl)
     // Handle any errors that occurred during the fetch
     console.error('Error fetching data:', error);
   });
-  
 // when we search for an item take the key word and put it into a string.
 // fetch image api and put it into query parameters
 // append image and desc from api
