86bc152b15bb1adbd39efb77dc75de029e13c3e7
ryanh <ryanhinson118@mail.com>
2024-04-16 13:34:26 -0400

    Added some Pseudo code to js files.

---
+++ assets/JS/favorites.js
@@ -0,0 +1,3 @@
+// get favorited items from localStorage
+// Append favorite items as cards
+// click card to expand to see in a wider view.
---
+++ assets/JS/image.js
@@ -0,0 +1,5 @@
+// when we search for an item take the key word and put it into a string.
+// fetch image api and put it into query parameters
+// append image and desc from api
+// add button to favorite that image and save to localStorage.
+// be able to search for another image.
