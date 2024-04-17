35e7be600dbeada7eac88b09001785cf30ef7fcb
ryanh <ryanhinson118@mail.com>
2024-04-17 13:41:54 -0400

    Added a materialize datepicker that took too long to work and too long to get tot he top of the page.

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,3 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.datepicker');
+  var instances = M.Datepicker.init(elems, {
+    format: 'yyyy-mm-dd'
+  });
+});
 // Have picture of the day on the main page.
 // make calendar widget where each click of the previous day shows that days image with the api.
 // make an error show up when they click a future date saying "This picture hasn't been posted yet." maybe a 404 picture.
