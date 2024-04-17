94ecf2d1de0f91ce3ba0c341d857480350678ccc
Hunter <hunterde91@gmail.com>
2024-04-17 12:29:50 -0400

    added rover information and created the cards

--- assets/JS/rover.js
+++ assets/JS/rover.js
@@ -1,4 +1,4 @@
-const testBody = $('body')
+const cardContainer = $('.cardContainer')
 //fetch rover api
 const roverAPI = function () {
     const apiUrl = 'https://api.nasa.gov/mars-photos/api/v1/rovers/perseverance/latest_photos?api_key=DEMO_KEY'
@@ -7,17 +7,33 @@ const roverAPI = function () {
         .then(function (response) {
             if (response.ok) {
                 response.json().then(function (data) {
-                    console.log(data);
-
+                    
                     //generate random number
                     let numElements = 5
                     for (let i = 0; i < numElements; i++) {
                         let randomNum = Math.floor(Math.random() * data.latest_photos.length)
-                        console.log(randomNum);
-                        // append images to page   
-                        const roverImg = testBody.append(`<img src= ${data.latest_photos[randomNum].img_src}></img>`)
-                        $(roverImg).attr(`src','${data.latest_photos[randomNum].img_src}`)
-                        $(testBody).append(roverImg)
+                        
+                         // create cards and append to page
+                        roverImg = `${data.latest_photos[randomNum].img_src}`
+                        pictureDate = `${data.latest_photos[randomNum].earth_date}`
+                        camera = `${data.latest_photos[randomNum].camera.full_name}`
+                        
+                         const roverCard = cardContainer.append(`
+                         <div class= 'col s2 m7'>
+                             <div class='card s1'>
+                                 <div class= card-image>
+                                     <img src='${roverImg}'>
+                                 </div>  
+                                 <div class= card-content>
+                                     <p>Date Image was taken: ${pictureDate}<br />Camera used to capture image: ${camera}</p>
+                                 </div> 
+                                 <div class= card-action>
+                                     <a href= #>placeholder</a>
+                                 </div>
+                             </div>
+                         </div>`)
+                       
+                        
                     }
                 })
             }
