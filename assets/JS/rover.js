035121bd9eeb029912ffac1f3e28ca071f396ddb
Hunter <hunterde91@gmail.com>
2024-04-17 10:02:56 -0400

    working on getting the rover api data

--- assets/JS/rover.js
+++ assets/JS/rover.js
@@ -1,20 +1,26 @@
 const testBody = $('body')
 //fetch rover api
 const roverAPI = function () {
-    const apiUrl = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=DEMO_KEY'
+    const apiUrl = 'https://api.nasa.gov/mars-photos/api/v1/rovers/perseverance/latest_photos?api_key=DEMO_KEY'
 
     fetch(apiUrl)
         .then(function (response) {
             if (response.ok) {
                 response.json().then(function (data) {
                     console.log(data);
-                    for (let i = 0; i < data.photos.length; i++) {
-                        const roverImg = testBody.append(`<img src= ${data.photos[i].img_src}></img>`)
-                        //$(roverImg).attr(`src','${data.photos[i].img_src}`)
-                        //$(testBody).append(roverImg)
+                    
+                    for (let i = 0; i < data.latest_photos.length; i++) {
+                        const roverImg = testBody.append(`<img src= ${data.latest_photos[i].img_src}></img>`)
+                        $(roverImg).attr(`src','${data.latest_photos[i].img_src}`)
+                        $(testBody).append(roverImg)
                     }
                 })
             }
         })
 }
 roverAPI()
+// Shuffle array
+const shuffled = array.sort(() => 0.5 - Math.random());
+
+// Get sub-array of first n elements after shuffled
+let selected = shuffled.slice(0, n);
